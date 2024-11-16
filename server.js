f332f806b3aa99e7caebf0b19f7b17ce33fb2790
zone <43332748+plateau11@users.noreply.github.com>
2024-11-16 23:03:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
     
     // Send a response back to the client
     //socket.emit('serverMessage', { message: 'Hello from server!' });
-    socket.emit('serverMessage', { message: data });
+    io.emit('serverMessage', { message: data });  //broadcasting message
   });
 
   // Handle disconnection
